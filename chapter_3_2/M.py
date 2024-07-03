662ab9e5a977f1c63896f82ac81fd01fab56d61f
Shoxrux Yuldashov <deagleshox@mail.ru>
2024-07-03 07:38:45 +0100

    Разбор ДЗ | Раздел 3.2
    - Разбор задач: `M` и `N

--- chapter_3_2/M.py
+++ chapter_3_2/M.py
@@ -1,15 +1,18 @@
 n: int = int(input())
-available_dishes: set[str] = set(input() for _ in range(n))
+available_dishes: set[str] = {input() for _ in range(n)}
 
 m: int = int(input())
 weekly_dishes: list = [set() for _ in range(m)]
+# weekly_dishes: set = set()
 
 for i in range(m):
     num_dishes: int = int(input())
     for _ in range(num_dishes):
         weekly_dishes[i].add(input())
+        # weekly_dishes.add(input())
 
 new_dishes: list[str] = sorted(available_dishes - set.union(*weekly_dishes))
+# new_dishes: list[str] = sorted(available_dishes - weekly_dishes)
 
 if new_dishes:
     print('\n'.join(new_dishes))
--- chapter_3_2/N.py
+++ chapter_3_2/N.py
@@ -1,12 +1,12 @@
 n: int = int(input())
-available_ingredients: set[str] = set(input() for _ in range(n))
+available_ingredients: set[str] = {input() for _ in range(n)}
 
 m: int = int(input())
 recipes: list[tuple[str, set[str]]] = []
 for _ in range(m):
     dish_name = input()
     num_ingredients = int(input())
-    recipe_ingredients = set(input() for _ in range(num_ingredients))
+    recipe_ingredients = {input() for _ in range(num_ingredients)}
     recipes.append((dish_name, recipe_ingredients))
 
 available_dishes: list[str] = []
@@ -18,3 +18,23 @@ if available_dishes:
     print('\n'.join(sorted(available_dishes)))
 else:
     print('Готовить нечего')
+
+# -------------------------------------------
+
+PRODUCTS = {input() for _ in range(int(input()))}
+
+RECEPTES = {}
+for _ in range(int(input())):
+    dish_name = input()
+    for _ in range(int(input())):
+        RECEPTES[dish_name] = RECEPTES.get(dish_name, []) + [input()]
+
+result = []
+for dish_name, recept in RECEPTES.items():
+    if set(recept) <= PRODUCTS:
+        result.append(dish_name)
+
+if result:
+    print(*sorted(result), sep="\n")
+else:
+    print("Готовить нечего")
